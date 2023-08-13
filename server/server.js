2c2de904c5ccb8e88c3029799647adf862feb14d
TymoTymo1 <79015481+TymoTymo1@users.noreply.github.com>
2023-08-13 23:10:12 +0200

    API working

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express()
 
-app.use(express.static('../client'));
+app.use(cors());
+app.get('/api', (req, res) => {
+    res.json({"users": ["userOne", "userTwo", "userThree"]});
+})
 
 app.listen(5000, () => {
     console.log('Server listening on port 5000');
